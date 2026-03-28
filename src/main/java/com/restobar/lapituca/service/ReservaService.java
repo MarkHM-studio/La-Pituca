9a3a6380db14cb0a4b1c1c44a5c371d6d4304f08
pokemon-battle-api <markhuamani5@gmail.com>
2026-03-28 15:44:49 -0500

    Se modificó el tiempo de expiración de una reserva

--- src/main/java/com/restobar/lapituca/service/ReservaService.java
+++ src/main/java/com/restobar/lapituca/service/ReservaService.java
@@ -149,7 +149,7 @@ public class ReservaService {
         reserva.setGrupo(grupo);
         reserva.setEstado("ESPERANDO PAGO");
         reserva.setFechaHora_expiracionPago(
-                LocalDateTime.now().plusMinutes(60)
+                LocalDateTime.now().plusMinutes(10)
         );
 
         reservaRepository.save(reserva);
